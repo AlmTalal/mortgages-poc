456ef2d398a5afac52dd140e8b1bf9b41f1b0b62
alm-talal <talal.alam@goquant.io>
2025-05-26 09:36:51 -0400

    Added max-height to fix Safari view

--- src/components/dashboard/MarketOverview.tsx
+++ src/components/dashboard/MarketOverview.tsx
@@ -177,7 +177,7 @@ export default function MarketOverview() {
 
       {/* Detailed Charts */}
       <Tabs defaultValue="mortgage-rates" className="w-full">
-        <TabsList className="grid w-full  grid-cols-2 md:grid-cols-4 gap-1 h-auto">
+        <TabsList className="grid w-full  grid-cols-2 md:grid-cols-4 gap-1 h-auto max-h-28">
           <TabsTrigger value="mortgage-rates" className="text-xs md:text-sm">
             Mortgage Rates
           </TabsTrigger>
